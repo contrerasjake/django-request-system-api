6083562e76c4bdcb7259d2d8ef7300a87b279844
JakeContreras <contrerasjaspherjake@gmail.com>
2021-06-06 18:22:34 +0800

    Added User Registration and JWT login

--- SystemCore/UsersApp/urls.py
+++ SystemCore/UsersApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListUser
+from .views import ListUser, RegisterView, LoginView
 
 urlpatterns = [
     path('', ListUser.as_view(), name='Users'),
     path('<int:pk>/', ListUser.as_view(), name='SingleUser'),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view())
 ]

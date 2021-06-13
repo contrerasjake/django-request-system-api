0caa428551b5d4d88a26c95b11c53aec09250358
JakeContreras <contrerasjaspherjake@gmail.com>
2021-06-13 17:23:33 +0800

    Added Logout + Code Refactoring

--- SystemCore/UsersApp/urls.py
+++ SystemCore/UsersApp/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserViewSet
-from .views import ListUser
+from .views import UserViewSet, LogoutAPIView
+from rest_framework_simplejwt import views as jwt_views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('logout/', LogoutAPIView.as_view(), name="logout"),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]

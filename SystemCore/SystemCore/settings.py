7cb8180e0d3c332ddf351b27dd6098cede2285a4
Jake Contreras <contreras.jaspherjake@gmail.com>
2022-03-05 23:03:15 +0800

    Update settings.py

--- SystemCore/SystemCore/settings.py
+++ SystemCore/SystemCore/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS=['.herokuapp.com', '127.0.0.1:8000']
 INTERNAL_IPS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-       'http://localhost:3000',
+       'http://127.0.0.1:3000',
 )
 
 

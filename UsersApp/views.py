35d63a3b1780e3dd8bae75f3fb75e4537bb1d407
contrerasjake <contreras.jaspherjake@gmail.com>
2022-05-23 13:03:18 +0800

    userinfo update request + password change request

--- UsersApp/views.py
+++ UsersApp/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .serializers import UserInformationSerializer, UserSerializer, LogoutSerializer, LoginSerializer
+from .serializers import UserInformationSerializer, UserSerializer, LogoutSerializer, LoginSerializer, ChangePasswordSerializer
 from .permissions import IsLoggedInUserOrAdmin, IsAdminUser
 from rest_framework.permissions import IsAuthenticated
 from .models import User
@@ -61,6 +61,24 @@ class LoginView(TokenObtainPairView):
 class UserInformationView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UserInformationSerializer
+    queryset = User.objects.all()
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = queryset.get(pk=self.request.user.id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
     def get(self, request):
         serializer = self.serializer_class(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = queryset.get(pk=self.request.user.id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer

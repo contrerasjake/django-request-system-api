95e4fa78664d75fb82f72be71eb6755d05a58fa1
millaresluis <millaresluis21@gmail.com>
2022-06-05 18:04:26 +0800

    fixed user model

--- UsersApp/models.py
+++ UsersApp/models.py
@@ -179,7 +179,7 @@ class User(AbstractBaseUser, PermissionsMixin):
             )
     email                   = models.EmailField(verbose_name="email", max_length=60, unique=True)
     date_joined             = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
-    last_login              = models.DateTimeField(verbose_name="last login", auto_now=True)
+    last_login              = models.DateTimeField(verbose_name="last login", auto_now=True, null=True)
     is_admin                = models.BooleanField(default=False)
     is_active               = models.BooleanField(default=True)
     is_email_verified       = models.BooleanField(default=False)
